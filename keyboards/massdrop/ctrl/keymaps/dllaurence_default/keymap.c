8dc337ef375722b24f02d567faebc55fb56345f6
Dustin Laurence <dustin@laurences.net>
2019-08-18 09:59:13 -0700

    L/R shift parens when tapped

--- keyboards/massdrop/ctrl/keymaps/dllaurence_default/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/dllaurence_default/keymap.c
@@ -38,7 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LALT_T(KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
 // Key  CAPS
         LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
-        KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                              KC_UP,            \
+// Key  LSHIFT                                                                                                     RSHIFT
+        KC_LSPO,         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,                              KC_UP,            \
         KC_LCTL,         KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(1),   KC_APP,  KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [1] = LAYOUT(
